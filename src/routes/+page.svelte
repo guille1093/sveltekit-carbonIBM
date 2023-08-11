5795797660804edd57239866c17af386c796af88
gq <guille121093@gmail.com>
2023-08-11 11:04:46 -0300

    tailwind added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<div class="items-center justify-end p-4 min-w-full flex-auto border-2 border-red-500">
+	<h1 class="text-3xl first-letter:text-5xl text-center">Welcome to SvelteKit</h1>
+</div>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +9,14 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
+
 };
 
 export default config;
+
