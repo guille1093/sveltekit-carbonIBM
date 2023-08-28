244eba1a1c5e44ff686c6304f8d2eb0c832ccd08
gq <guille121093@gmail.com>
2023-08-28 09:37:21 -0300

    ventas iniciado

--- src/routes/ventas/+page.svelte
+++ src/routes/ventas/+page.svelte
@@ -4,18 +4,3 @@
 </script>
 
 <h1>Ventas</h1>
-
-{#if data && data.total > 0}
-	{#each data.ventas as venta}
-		<p>{venta.id} - {venta.fecha} - {venta.total}</p>
-	{/each}
-	<p>Hay {data.total} ventas</p>
-{/if}
-
-{#if data && data.total === 0}
-	<p>No hay ventas</p>
-{/if}
-
-{#if data && data.total === undefined}
-	<p>Cargando...</p>
-{/if}
