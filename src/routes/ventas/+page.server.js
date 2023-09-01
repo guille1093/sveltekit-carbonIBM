e95f320ee95abaaccaec175d17ec85f36169c431
gq <guille121093@gmail.com>
2023-08-31 22:39:18 -0300

    multiselect pasajeros

--- src/routes/ventas/+page.server.js
+++ src/routes/ventas/+page.server.js
@@ -1,4 +1,70 @@
 /** @type {import('./$types').PageServerLoad} */
-export async function load() {
-    return {};
+export async function load({ locals }) {
+    //retonar todos los clientes y los paquetes
+
+    try {
+        const getClientes = async () => {
+            // @ts-ignore
+            return structuredClone(await locals.pb.collection('clientes').getFullList(5000, {}));
+        };
+
+        const getPaquetes = async () => {
+            // @ts-ignore
+            return structuredClone(await locals.pb.collection('projects').getFullList(undefined, {}));
+        };
+
+        const [clientes, paquetes] = await Promise.all([getClientes(), getPaquetes()]);
+        console.log('clientes y paquetes cargados');
+
+        return {
+            clientes,
+            paquetes
+        };
+    } catch (err) {
+        console.log('Error: ', err);
+    }
+};
+
+export const actions = {
+    // example create data
+
+    //     "cliente": "RELATION_RECORD_ID",
+    //     "paquete": "RELATION_RECORD_ID",
+    //     "pagado": "test",
+    //     "observaciones": "test",
+    //     "estado": "test",
+    //     "cant_personas": 123,
+    //     "pasajeros": [
+    //         "ID",
+    //         "ID",
+    //     ]
+
+    createVenta: async ({ request, locals }) => {
+        await new Promise((fulfil) => setTimeout(fulfil, 1500));
+        const form = await request.formData();
+        console.log('form: ', form);
+        const cliente = form.get('cliente') ?? '';
+        const paquete = form.get('paquete') ?? '';
+        const pagado = form.get('pagado') ?? '';
+        const observaciones = form.get('observaciones') ?? '';
+        const estado = form.get('estado') ?? '';
+        const cant_personas = form.get('cant_personas') ?? '';
+        const pasajeros = form.get('pasajeros') ?? '';
+        console.log('pasajeros: ', pasajeros);
+        const data = {
+            cliente,
+            paquete,
+            pagado,
+            observaciones,
+            estado,
+            cant_personas,
+            pasajeros: pasajeros.toString().split(',')
+        };
+        console.log('data: ', data);
+        const { id } = await locals.pb.collection('ventas').create(data);
+        return {
+            status: 303,
+            redirect: `/ventas/${id}`
+        };
+    }
 };
--- src/routes/ventas/+page.svelte
+++ src/routes/ventas/+page.svelte
@@ -1,6 +1,89 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	export let data;
+
+	import { MultiSelect } from 'carbon-components-svelte';
+	import {
+		StructuredList,
+		StructuredListHead,
+		StructuredListRow,
+		StructuredListCell,
+		StructuredListBody
+	} from 'carbon-components-svelte';
+	import { Tile } from 'carbon-components-svelte';
+	import { Grid, Row, Column } from 'carbon-components-svelte';
+	/**
+	 * @type {never[]}
+	 */
+	let selected = [];
+	let items = data.clientes.map((/** @type {{ id: any; nombre: any; }} */ cliente) => ({
+		id: cliente.id,
+		text: cliente.nombre
+	}));
+	let form;
 </script>
 
 <h1>Ventas</h1>
+
+<Grid>
+	<Row>
+		<Column>
+			<h2>Crear venta</h2>
+			<Tile>
+				<form method="post" action="?/createVenta" bind:this={form}>
+					<input type="hidden" name="pasajeros" value={selected} />
+
+					<MultiSelect
+						filterable
+						bind:selectedIds={selected}
+						titleText="Acompañantes"
+						label="Seleccione los acompañantes"
+						{items}
+					/>
+					<button type="submit">Enviar</button>
+				</form>
+			</Tile>
+		</Column>
+		<Column>
+			<Tile>
+				{#if selected.length > 0}
+					<h2>Seleccionados</h2>
+					<StructuredList>
+						<StructuredListHead>
+							<StructuredListRow>
+								<StructuredListCell head>Nombre</StructuredListCell>
+								<StructuredListCell head>Apellido</StructuredListCell>
+								<StructuredListCell head>DNI</StructuredListCell>
+								<StructuredListCell head>Seleccionar</StructuredListCell>
+							</StructuredListRow>
+						</StructuredListHead>
+						<StructuredListBody>
+							{#each selected as id}
+								{#each data.clientes as cliente}
+									{#if cliente.id === id}
+										<StructuredListRow>
+											<StructuredListCell>{cliente.nombre}</StructuredListCell>
+											<StructuredListCell>{cliente.apellido}</StructuredListCell>
+											<StructuredListCell>{cliente.dni}</StructuredListCell>
+											<StructuredListCell>
+												<button
+													on:click={() => {
+														selected = selected.filter(
+															(/** @type {any} */ id) => id !== cliente.id
+														);
+													}}
+												>
+													Eliminar
+												</button>
+											</StructuredListCell>
+										</StructuredListRow>
+									{/if}
+								{/each}
+							{/each}
+						</StructuredListBody>
+					</StructuredList>
+				{/if}
+			</Tile>
+		</Column>
+	</Row>
+</Grid>
