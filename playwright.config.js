17f527d6cd65812729f5a931d54b0832c2620850
guille1093 <guille121093@gmail.com>
2023-09-20 15:19:58 -0300

    tests timeout

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ const config = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		url: 'http://localhost:4173',
-		timeout: 120 * 2000
+		timeout: 120 * 3000
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
