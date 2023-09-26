57a7c15e4eca3a37bdddc21abcd7a5efd0a351e0
guille1093 <guille121093@gmail.com>
2023-09-26 14:33:56 -0300

    vercel-adapter para deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({polyfill: false})
+		adapter: adapter()
 	},
 	preprocess: [
 		preprocess({
