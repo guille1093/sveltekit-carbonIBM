4a1e4bfabf99cdc5032d00a15e0328a3c9ff6e82
guille1093 <guille121093@gmail.com>
2023-09-27 13:20:24 -0300

    code smells

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,10 @@
 import PocketBase from 'pocketbase';
 
 export const handle = async ({ event, resolve }) => {
-	// console.log('handle', event);
     
 	// Conectamos con la base de datos
 	event.locals.pb = new PocketBase('https://gq-pfs.pockethost.io');
-	// console.log('pb', event.locals.pb);
+	
     
 	// Cargamos la cookie de autenticación
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
