e2eb2be10202f23196348fc6dfbc80696366355a
guille1093 <guille121093@gmail.com>
2023-10-04 14:11:45 -0300

    color para gente "normal"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.postcss';
-	import 'carbon-components-svelte/css/g100.css';
+	import 'carbon-components-svelte/css/g10.css';
 	import { page } from '$app/stores';
 	import {
 		Header,
