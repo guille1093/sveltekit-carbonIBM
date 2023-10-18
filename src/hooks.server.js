66cb1a5ed5c7b60d59bd7e454105b04d15591fa0
guille1093 <guille121093@gmail.com>
2023-10-18 14:23:34 -0300

    otro millon de cambios

---
+++ src/hooks.server.js
@@ -0,0 +1,28 @@
+import PocketBase from 'pocketbase';
+
+export const handle = async ({ event, resolve }) => {
+	// Conectamos con la base de datos
+	event.locals.pb = new PocketBase('https://gq-pfs.pockethost.io');
+
+	// Cargamos la cookie de autenticación
+	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+
+	// Si la cookie es válida, la refrescamos y guardamos el usuario en locals
+	try {
+		if (event.locals.pb.authStore.isValid) {
+			await event.locals.pb.collection('users').authRefresh();
+			event.locals.user = structuredClone(event.locals.pb.authStore.model);
+		}
+		// Si no es válida, la borramos
+	} catch (_) {
+		event.locals.pb.authStore.clear();
+		event.locals.user = undefined;
+	}
+
+	// Ejecutamos el resto de hooks y la ruta solicitada
+	const response = await resolve(event);
+
+	// Guardamos la cookie de autenticación
+	response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: true }));
+	return response;
+};
---
+++ src/lib/components/packageCard.svelte
@@ -0,0 +1,149 @@
+<script>
+	import {
+		Button,
+		ComposedModal,
+		ModalHeader,
+		ModalBody,
+		ClickableTile,
+		StructuredList,
+		StructuredListBody,
+		StructuredListRow,
+		StructuredListCell,
+		AspectRatio
+	} from 'carbon-components-svelte';
+
+	import { getImageURL } from '$lib/utils';
+	/**
+	 * @type {{ precio: number | bigint; fechasalida: string | number | Date; fecharetorno: string | number | Date; cant_dias: any; cant_noches: any; id: any; nombre: any; }}
+	 */
+	export let project;
+	let pesoARLocale = Intl.NumberFormat('es-AR', {
+		style: 'currency',
+		currency: 'ARS',
+		minimumFractionDigits: 0
+	});
+	const precio = pesoARLocale.format(project.precio);
+	let fechaSalida = new Date(project.fechasalida).toLocaleDateString('es-ES', {
+		day: '2-digit',
+		month: 'short',
+		year: 'numeric'
+	});
+
+	let fechaRetorno = new Date(project.fecharetorno).toLocaleDateString('es-ES', {
+		day: '2-digit',
+		month: 'short',
+		year: 'numeric'
+	});
+
+	const items = [
+		{
+			name: 'Salida',
+			icon: 'bx-calendar-event',
+			value: fechaSalida
+		},
+		{
+			name: 'Llegada',
+			icon: 'bx-calendar-check',
+			value: fechaRetorno
+		}
+	];
+	let open = false;
+
+	import { truncate } from 'carbon-components-svelte';
+</script>
+
+<div class="hover:scale-125 hover:z-50 transform transition-all duration-500 ease-in-out w-[320px]">
+	<ClickableTile on:click={() => (open = true)}>
+		{#if project.thumbnail}
+			<AspectRatio ratio="16x9">
+				<img
+					class="h-[220px] object-cover"
+					src={getImageURL(project.collectionId, project.id, project.thumbnail)}
+					alt="logo"
+				/>
+			</AspectRatio>
+		{/if}
+		<div class="mt-16">
+			<h3 use:truncate class="">
+				{project.nombre}
+			</h3>
+
+			<div class="flex items-baseline mb-4">
+				<span class="" />
+				<h4>{precio}</h4>
+			</div>
+			<!-- List -->
+			<ul role="list" class="space-y-4">
+				{#each items as item}
+					<li class="flex space-x-3 justify-between">
+						<div>
+							<!-- Icon -->
+							<i class="bx text-blue-600 {item.icon}" />
+							<!-- Name -->
+							<span>{item.name}:</span>
+						</div>
+
+						<!-- Text -->
+						<span>{item.value}</span>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</ClickableTile>
+</div>
+
+<ComposedModal bind:open>
+	<ModalHeader>
+		<h2 class="text-2xl font-semibold">
+			{project.nombre}
+		</h2>
+		<p>
+			{precio}
+		</p>
+	</ModalHeader>
+
+	<ModalBody>
+		<StructuredList condensed>
+			<StructuredListBody>
+				<StructuredListRow>
+					<StructuredListCell noWrap><strong>Nombre del paquete:</strong></StructuredListCell>
+					<StructuredListCell>{project.nombre}</StructuredListCell>
+				</StructuredListRow>
+				<StructuredListRow>
+					<StructuredListCell noWrap><strong>Pais destino:</strong></StructuredListCell>
+					<StructuredListCell>{project.pais_destino}</StructuredListCell>
+				</StructuredListRow>
+
+				<StructuredListRow>
+					<StructuredListCell noWrap><strong>Regimen:</strong></StructuredListCell>
+					<StructuredListCell>{project.regimen}</StructuredListCell>
+				</StructuredListRow>
+
+				<StructuredListRow>
+					<StructuredListCell noWrap><strong>Salida:</strong></StructuredListCell>
+					<StructuredListCell>{fechaSalida}</StructuredListCell>
+				</StructuredListRow>
+				<StructuredListRow>
+					<StructuredListCell noWrap><strong>Llegada:</strong></StructuredListCell>
+					<StructuredListCell>{fechaRetorno}</StructuredListCell>
+				</StructuredListRow>
+				<StructuredListRow>
+					<StructuredListCell noWrap><strong>Dias:</strong></StructuredListCell>
+					<StructuredListCell>{project.cant_dias}</StructuredListCell>
+				</StructuredListRow>
+				<StructuredListRow>
+					<StructuredListCell noWrap><strong>Noches:</strong></StructuredListCell>
+					<StructuredListCell>{project.cant_noches}</StructuredListCell>
+				</StructuredListRow>
+				<StructuredListRow>
+					<StructuredListCell noWrap><strong>Descripción:</strong></StructuredListCell>
+					<StructuredListCell>{project.descripcion}</StructuredListCell>
+				</StructuredListRow>
+			</StructuredListBody>
+		</StructuredList>
+		<div class="flex justify-end">
+			<Button kind="primary" href="/paquetes/{project.id}">Ver más</Button>
+			<Button kind="secondary" on:click={() => (open = false)}>Cerrar</Button>
+		</div>
+	</ModalBody>
+</ComposedModal>
