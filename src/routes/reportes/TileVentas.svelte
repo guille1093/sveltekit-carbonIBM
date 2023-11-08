3c69ecec8c586277e428d85d7200bceac537b91c
guille1093 <guille121093@gmail.com>
2023-11-08 13:57:36 -0300

    null filter from data table

--- src/routes/reportes/TileVentas.svelte
+++ src/routes/reportes/TileVentas.svelte
@@ -63,6 +63,10 @@
 			};
 		}
 	});
+
+//eliminar pagos nulls de row
+	rows = rows.filter((row) => row !== undefined);
+
 	let pageSize = 5;
 	let page = 1;
 	// fin de las cosas para pasar a la tabla xd
